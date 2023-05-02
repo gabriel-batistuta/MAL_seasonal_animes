26c366dfe10228b59bd1f4eed0e214382e5fd7d2
gabriel <batistutag190@gmail.com>
2023-05-02 15:51:54 -0300

    update folder reoved functions for delete folders that doesnt exists

--- modules/folder.py
+++ modules/folder.py
@@ -7,21 +7,8 @@ def createFolder():
     else:
         os.mkdir('animes')
 
-def createTemplateFolder():
-    if os.path.isdir('animes/templates'):
-        pass
-    else:
-        os.mkdir('animes/templates')
-
 def removeFolder():
     shutil.rmtree('./animes', ignore_errors=True)
 
-def removeHtmlFile():
-    path = 'animes/templates'
-    shutil.rmtree(path, ignore_errors=True)
-
-def removePdfFile():
-    path = './animes'
-    shutil.rmtree(path, ignore_errors=True)
-
-removePdfFile()
+if __name__ == '__main__':
+    removeFolder()
