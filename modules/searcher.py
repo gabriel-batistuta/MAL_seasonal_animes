5b6a92349b93176b23e5b05d6e564e58ee5a3b62
gabriel <batistutag190@gmail.com>
2023-05-02 15:48:50 -0300

    update searcher module, rename: headers to header

--- modules/searcher.py
+++ modules/searcher.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-def getAnimes(url, headers):
+def getAnimes(url, header):
 
-    response = requests.get(url,headers=headers).content
+    response = requests.get(url,headers=header).content
     
     json_data = json.loads(response)
 
@@ -18,7 +18,7 @@ def getAnimes(url, headers):
             animeList.append(anime)
         try:
             nextPage = json_data['paging']
-            json_data = requests.get(nextPage['next'],headers=headers).content
+            json_data = requests.get(nextPage['next'],headers=header).content
             json_data = json.loads(json_data)
             animes = json_data['data']
         except:
