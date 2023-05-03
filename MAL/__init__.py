772847dbf438e295670bb42e496891889684bac2
gabriel <batistutag190@gmail.com>
2023-05-02 22:16:08 -0300

    created __init__.py

--- MAL/__init__.py
+++ MAL/__init__.py
@@ -2,3 +2,4 @@ from .filter import getAnimeDetails, getCurrentSeason
 from .folder import createFolder, removeFolder
 from .pdf import create_pdf
 from .searcher import getAnimes
+from .app import menu
